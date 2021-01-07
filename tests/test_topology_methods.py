1faa58421eaa0490c1e395cda1967db562878356
Eleftherios Zisis <eleftherios.zisis@epfl.ch>
2021-01-07 10:15:35 +0100

    Refactor Topology methods

--- tests/test_topology_methods.py
+++ tests/test_topology_methods.py
@@ -1,6 +1,7 @@
 '''Test tmd.topology.methods'''
-from nose import tools as nt
+import mock
 import numpy as np
+from numpy import testing as npt
 from tmd.Topology import methods
 from tmd.Topology import analysis
 from tmd.Tree import Tree
@@ -67,15 +68,16 @@ tree1 = Tree.Tree(x=x2, y=y2, z=z2, d=d2, t=t2, p=p2)
 def test_get_persistence_diagram():
     ph0 = methods.get_persistence_diagram(tree0)
     ph1 = methods.get_persistence_diagram(tree1)
-    nt.ok_(np.allclose(ph0, [[12.24744871391589, 11.180339887498949],
+
+    npt.assert_allclose(ph0, [[12.24744871391589, 11.180339887498949],
                              [11.180339887498949, 10.0],
-                             [12.24744871391589, 0]]))
-    nt.ok_(np.allclose(ph1, [[11.180339887498949, 10.0],
-                             [11.180339887498949, 0]]))
+                             [12.24744871391589, 0]])
+    npt.assert_allclose(ph1, [[11.180339887498949, 10.0],
+                             [11.180339887498949, 0]])
     ph0 = methods.get_persistence_diagram(tree0, feature='path_distances')
     ph1 = methods.get_persistence_diagram(tree1, feature='path_distances')
-    nt.ok_(np.allclose(ph0, [[20.0, 15.0], [15.0, 10.0], [20.0, 0]]))
-    nt.ok_(np.allclose(ph1, [[15.0, 10.0], [15.0, 0]]))
+    npt.assert_allclose(ph0, [[20.0, 15.0], [15.0, 10.0], [20.0, 0]])
+    npt.assert_allclose(ph1, [[15.0, 10.0], [15.0, 0]])
 
 def test_extract_persistence_diagram():
     import filecmp
@@ -84,43 +86,186 @@ def test_extract_persistence_diagram():
     methods.extract_ph(tree0, output_file='./test_ph.txt')
     ph_file = analysis.load_file('./test_ph.txt')
     ph_0 = analysis.load_file(sample_ph_0)
-    nt.ok_(np.allclose(ph_file, ph_0))
+    npt.assert_allclose(ph_file, ph_0)
     os.remove('test_ph.txt')
 
 def test_get_lifetime():
     lf0 = methods.get_lifetime(tree0)
     lf1 = methods.get_lifetime(tree1)
-    nt.ok_(np.allclose(lf0, np.array([[  0.        ,  10.        ],
+    npt.assert_allclose(lf0, np.array([[  0.        ,  10.        ],
                                       [ 10.        ,  11.18033989],
                                       [ 10.        ,  11.18033989],
                                       [ 11.18033989,  12.24744871],
-                                      [ 11.18033989,  12.24744871]])))
-    nt.ok_(np.allclose(lf1, np.array([[  0.        ,  10.        ],
+                                      [ 11.18033989,  12.24744871]]))
+    npt.assert_allclose(lf1, np.array([[  0.        ,  10.        ],
                                       [ 10.        ,  11.18033989],
-                                      [ 10.        ,  11.18033989]])))
+                                      [ 10.        ,  11.18033989]]))
 
 def test_extract_connectivity_from_points():
     dist = methods.extract_connectivity_from_points(tree, threshold=0.1)
-    nt.ok_(np.allclose(dist, np.array([[ True, False, False, False, False],
+    npt.assert_allclose(dist, np.array([[ True, False, False, False, False],
                                        [False,  True, False, False, False],
                                        [False, False,  True, False, False],
                                        [False, False, False,  True, False],
-                                       [False, False, False, False,  True]], dtype=bool)))
+                                       [False, False, False, False,  True]], dtype=bool))
     dist = methods.extract_connectivity_from_points(tree, threshold=1.1)
-    nt.ok_(np.allclose(dist, np.array([[ True,  True,  True, False, False],
+    npt.assert_allclose(dist, np.array([[ True,  True,  True, False, False],
                                        [ True,  True, False,  True, False],
                                        [ True, False,  True, False, False],
                                        [False,  True, False,  True, False],
-                                       [False, False, False, False,  True]], dtype=bool)))
+                                       [False, False, False, False,  True]], dtype=bool))
     dist = methods.extract_connectivity_from_points(tree, threshold=2.1)
-    nt.ok_(np.allclose(dist, np.array([[ True,  True,  True,  True,  True],
+    npt.assert_allclose(dist, np.array([[ True,  True,  True,  True,  True],
                                        [ True,  True,  True,  True,  True],
                                        [ True,  True,  True, False,  True],
                                        [ True,  True, False,  True,  True],
-                                       [ True,  True,  True,  True,  True]], dtype=bool)))
+                                       [ True,  True,  True,  True,  True]], dtype=bool))
     dist = methods.extract_connectivity_from_points(tree, threshold=3.1)
-    nt.ok_(np.allclose(dist, np.array([[ True,  True,  True,  True,  True],
+    npt.assert_allclose(dist, np.array([[ True,  True,  True,  True,  True],
                                        [ True,  True,  True,  True,  True],
                                        [ True,  True,  True,  True,  True],
                                        [ True,  True,  True,  True,  True],
-                                       [ True,  True,  True,  True,  True]], dtype=bool)))
+                                       [ True,  True,  True,  True,  True]], dtype=bool))
+
+
+def test_node_connectivity():
+
+    tree = mock.Mock()
+    tree.p = [-1]
+
+    edges = np.array([
+        [0, 2],
+        [2, 3],
+        [2, 5]
+    ])
+
+    beg = edges[:, 0]
+    end = edges[:, 1]
+
+    parents, children = methods._node_connectivity(tree, beg, end)
+
+    assert parents == {0: -1, 2: 0, 3: 2, 5: 2}
+
+    expected_children = {0: [2], 2: [3, 5]}
+
+    for key, values in expected_children.items():
+        npt.assert_array_equal(children[key], values)
+
+
+def test_filtration_function():
+
+    feature = 'radial_distances'
+    func = methods._filtration_function(feature)
+    npt.assert_allclose(func(tree), [0. , 1. , 1., 1.41421356, 1.41421356])
+
+    feature = 'path_distances'
+    func = methods._filtration_function(feature)
+    npt.assert_allclose(func(tree), [0., 1., 3., 5.236068, 6.650282])
+
+
+def test_tree_to_property_barcode():
+
+    filtration_function = lambda tree: np.array([0., 1., 3., 5., 7.])
+
+    prop = lambda *args: mock.Mock(
+        get=lambda v: [],
+        infinite_component=lambda v: []
+    )
+
+    ph = methods.tree_to_property_barcode(tree_trifork, filtration_function, prop)
+
+    npt.assert_array_equal(ph, [
+        [3., 1.],
+        [5., 1.],
+        [7., 0.],
+    ])
+
+    prop = lambda *args: mock.Mock(
+        get=lambda v: [v],
+        infinite_component=lambda v: [0]
+    )
+
+    ph = methods.tree_to_property_barcode(tree_trifork, filtration_function, prop)
+
+    npt.assert_array_equal(ph, [
+        [3., 1., 1.],
+        [5., 1., 1.],
+        [7., 0., 0.],
+    ])
+
+    prop = lambda *args: mock.Mock(
+        get=lambda v: list(range(v, v + 5)),
+        infinite_component=lambda v: [np.nan] * 5
+    )
+
+    ph = methods.tree_to_property_barcode(tree_trifork, filtration_function, prop)
+
+    npt.assert_array_equal(ph, [
+        [3., 1., 1., 2., 3., 4., 5.],
+        [5., 1., 1., 2., 3., 4., 5.],
+        [7., 0., np.nan, np.nan, np.nan, np.nan, np.nan],
+    ])
+
+
+def _integration_tree():
+
+    xyzrtp = np.array([
+       [  5.,  -2.,   5.,   0.1,   3.,  -1.],
+       [ 11.,  -5.,  12.,   0.1,   3.,   0.],
+       [ 18.,  -8.,  19.,   0.2,   3.,   1.],
+       [ 24., -11.,  26.,   0.1,   3.,   2.],
+       [ 31., -14.,  33.,   0.2,   3.,   3.],
+       [ 38., -16.,  40.,   0.3,   3.,   4.],
+       [ 45., -18.,  47.,   0.4,   3.,   5.],
+       [ 52., -19.,  53.,   0.3,   3.,   6.],
+       [ 37., -17.,  39.,   0.2,   3.,   4.],
+       [ 44., -20.,  46.,   0.1,   3.,   8.],
+       [ 50., -23.,  53.,   0.1,   3.,   9.],
+       [ 57., -26.,  60.,   0.2,   3.,  10.],
+       [ 63., -29.,  67.,   0.3,   3.,  11.],
+       [ 70., -32.,  74.,   0.4,   3.,  12.],
+       [ 24., -10.,  26.,   0.5,   3.,   2.],
+       [ 31., -12.,  33.,   0.4,   3.,  14.],
+       [ 39., -13.,  40.,   0.3,   3.,  15.]])
+
+    return Tree.Tree(
+        x=xyzrtp[:, 0], y=xyzrtp[:, 1], z=xyzrtp[:, 2],
+        d=xyzrtp[:, 3], t=xyzrtp[:, 4].astype(int), p=xyzrtp[:, 5].astype(int)
+    )
+
+
+def test_get_persistence_diagram__integration():
+
+    npt.assert_allclose(
+        methods.get_persistence_diagram(_integration_tree()),
+        [
+            [69.29646455628166, 40.049968789001575],
+            [50.0199960015992, 20.024984394500787],
+            [99.42836617384397, 0]
+        ]
+    )
+
+
+def test_get_ph_angles__integration():
+
+    npt.assert_allclose(
+        methods.get_ph_angles(_integration_tree()),
+        [
+            [69.296464556281, 40.0499687890015, 0.0, 0.012041319646904, 0.19866459470163, 0.0148790141866820],
+            [50.01999600159, 20.0249843945007, 0.0, 0.0, 0.198664594701636, 0.0025605561025675],
+            [99.428366173843, 0, np.nan, np.nan, np.nan, np.nan]
+        ]
+    )
+
+
+def test_get_ph_radii__integration():
+
+    npt.assert_allclose(
+        methods.get_ph_radii(_integration_tree()),
+        [
+            [69.29646455628166, 40.049968789001575, 0.3],
+            [50.0199960015992, 20.024984394500787, 0.15],
+            [99.42836617384397, 0, 0.1]
+        ]
+    )
+
---
+++ tests/test_topology_persistent_properties.py
@@ -0,0 +1,192 @@
+from mock import patch, Mock
+import numpy as np
+from numpy import testing as npt
+from tmd.Topology import persistent_properties as tested
+
+
+class MockTree:
+
+    def __init__(self, points):
+
+        self._points = points
+
+    def get_direction_between(self, start_id, end_id):
+        vec = self._points[end_id] - self._points[start_id]
+        vec /= np.linalg.norm(vec)
+        return vec
+
+
+def test_no_property():
+
+    prop = tested.NoProperty(None)
+
+    res_get = prop.get(None)
+    res_inf = prop.infinite_component(None)
+
+    assert isinstance(res_get, list)
+    assert isinstance(res_inf, list)
+
+    npt.assert_equal(len(res_get), 0)
+    npt.assert_equal(len(res_inf), 0)
+
+
+def test_persistent_angles():
+
+    tree = MockTree(
+        np.array([
+            [0., 0., 1.],
+            [0., 0., 0.],
+            [0., 0., -1.],
+            [0., 1., 0.]
+        ])
+    )
+
+    begs = [-1, 1]
+
+    children = {1: [2, 3]}
+    parents = {1: 0}
+
+    prop = tested.PersistentAngles(tree, begs, None, parents, children)
+
+    res_get = prop.get(1)
+    res_inf = prop.infinite_component(0)
+
+    assert isinstance(res_get, list)
+    assert isinstance(res_inf, list)
+
+    npt.assert_allclose(res_get, [0., 0., 0.5 * np.pi, -0.5 * np.pi])
+    npt.assert_array_equal(res_inf, [np.nan, np.nan, np.nan, np.nan])
+
+
+def test_section_mean_radii():
+
+    radii = np.array([0.1, 0.2, 0.3, 0.4, 0.5, 0.6])
+
+    section_begs = np.array([0, 2, 5])
+    section_ends = np.array([2, 5, 6])
+
+    expected_mean_radii = [0.15, 0.4, 0.6]
+
+    npt.assert_allclose(
+        tested._section_mean_radii(radii, section_begs, section_ends),
+        expected_mean_radii
+    )
+
+
+def test_persistent_mean_radius():
+
+    tree = MockTree(
+        np.array([
+            [0., 0., 1.],
+            [0., 0., 0.],
+            [0., 0., -1.],
+            [0., 1., 0.]
+        ])
+    )
+    tree.d = [0.1, 0.2, 0.3, 0.4]
+
+    # get one value per section
+    section_begs = np.array([0, 1, 2, 3])
+    section_ends = section_begs + 1
+
+    prop = tested.PersistentMeanRadius(tree, section_begs, section_ends)
+
+    res_get = prop.get(2)
+    res_inf = prop.infinite_component(0)
+
+    assert isinstance(res_get, list)
+    assert isinstance(res_inf, list)
+
+    npt.assert_array_equal(res_get, [0.3])
+    npt.assert_array_equal(res_inf, [0.1])
+
+
+def test_phi_theta():
+
+    u = np.array([1., 0., 0.])
+    v = np.array([1., 0., 0.])
+
+    npt.assert_allclose(tested._phi_theta(u, v), [0., 0.])
+    npt.assert_allclose(tested._phi_theta(v, u), [0., 0.])
+    npt.assert_allclose(tested._phi_theta(3. * u, 4. * v), [0., 0.])
+
+    u = np.array([1., 0., 0.])
+    v = np.array([0., 0., 1.])
+
+    npt.assert_allclose(tested._phi_theta(u, v), [0., -np.pi * 0.5])
+    npt.assert_allclose(tested._phi_theta(v, u), [0., +np.pi * 0.5])
+    npt.assert_allclose(tested._phi_theta(2. * v, 3. * u), [0., np.pi * 0.5])
+
+    u = np.array([1., 0., 0.])
+    v = np.array([-1., 0., 0.])
+
+    npt.assert_allclose(tested._phi_theta(u, v), [np.pi, 0.])
+    npt.assert_allclose(tested._phi_theta(v, u), [-np.pi, 0.])
+    npt.assert_allclose(tested._phi_theta(2. * v, 3. * u), [-np.pi, 0.])
+
+    u = np.array([1., 1., 0.])
+    v = np.array([1., 0., 1.])
+
+    npt.assert_allclose(tested._phi_theta(u, v), [-0.25 * np.pi, -0.25 * np.pi])
+    npt.assert_allclose(tested._phi_theta(v, u), [0.25 * np.pi, 0.25 * np.pi])
+    npt.assert_allclose(tested._phi_theta(2. * v, 3. * u), [0.25 * np.pi, 0.25 * np.pi])
+
+
+def test_angles_tree():
+
+    tree = MockTree(
+        np.array([
+            [0., 0., 1.],
+            [0., 0., 0.],
+            [1., 0., 0.],
+            [0., 1., 0.]
+        ])
+    )
+
+    angles = tested._angles_tree(tree, parID=0, parEND=1, ch1ID=2, ch2ID=3)
+    npt.assert_allclose(angles, [0., -0.5 * np.pi, 0.5 * np.pi, 0.])
+
+    tree = MockTree(
+        np.array([
+            [0., 0., 1.],
+            [0., 0., 0.],
+            [0., 0., -1.],
+            [0., 1., 0.]
+        ])
+    )
+
+    angles = tested._angles_tree(tree, parID=0, parEND=1, ch1ID=2, ch2ID=3)
+    npt.assert_allclose(angles, [0., 0., 0.5 * np.pi, -0.5 * np.pi])
+
+    tree = MockTree(
+        np.array([
+            [0., 0., 1.],
+            [0., 0., 0.],
+            [0., 1., 0.],
+            [0., 0., -1.]
+        ])
+    )
+
+    angles = tested._angles_tree(tree, parID=0, parEND=1, ch1ID=2, ch2ID=3)
+    npt.assert_allclose(angles, [0., 0., 0.5 * np.pi, -0.5 * np.pi])
+
+
+def test_get_angles():
+
+    tree = MockTree(
+        np.array([
+            [0., 0., 1.],
+            [0., 0., 0.],
+            [0., 0., -1.],
+            [0., 1., 0.]
+        ])
+    )
+
+    begs = [-1, 1]
+
+    children = {1: [2, 3]}
+    parents = {1: 0}
+
+    angles = tested.get_angles(tree, begs, parents, children)
+    npt.assert_allclose(angles, [[0., 0., 0., 0.], [0., 0., 0.5 * np.pi, -0.5 * np.pi]])
+
