1fef6e7e0d96c38dbbb56ba9167ad1f99ab3fe06
kanari <lida.kanari@epfl.ch>
2023-05-19 14:43:37 +0200

    Fix issues

--- tmd/Topology/distances.py
+++ tmd/Topology/distances.py
@@ -15,8 +15,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-# pylint: disable=invalid-slice-index
-
 import numpy as np
 
 from .vectorizations import betti_curve
