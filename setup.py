e68b12a89a2ed3eb136ac44ad2dc47914a2bd474
Adrien Berchet <adrien.berchet@gmail.com>
2023-09-28 12:03:57 +0200

    Build: Drop support for Python 3.7

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ setup(
     },
     license="GNU Lesser General Public License v3.0",
     packages=find_namespace_packages(include=["tmd*"]),
-    python_requires=">=3.7",
+    python_requires=">=3.8",
     use_scm_version=True,
     setup_requires=[
         "setuptools_scm",
@@ -59,7 +59,6 @@ setup(
         "Intended Audience :: Science/Research",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
